b0c9e9f8cdbabd5840dc40d6f67f6958126fd5c0
Muhyideen Akindele <51252833+TheAkindele@users.noreply.github.com>
2021-05-28 08:02:22 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className="text-blue-500 h-screen w-full flex justify-center items-center">
+      React, Tailwind Css and scss setup template
+    </div>
+  );
+}
+
+export default App;
